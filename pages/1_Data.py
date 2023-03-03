5d5f238e42f2b68ef47323787789cf7a7b9dbc2f
Collinul <calin.tianu1@gmail.com>
2023-03-03 17:50:27 +0200

    Added multiple pdf functionality

--- pages/1_Data.py
+++ pages/1_Data.py
@@ -1,33 +1,26 @@
 import streamlit as st
 from PyPDF2 import PdfReader
 import pandas as pd
+import os
+import numpy as np
 
-
-#site config
-st.set_page_config(page_title="Extract", layout="wide",  page_icon=":sun_with_face:")
+# site config
+st.set_page_config(page_title="Extract", layout="wide",
+                   page_icon=":sun_with_face:")
 st.header("Aici ma joc eu cu datele , nu baga in seama  ")
 st.title("Data")
-#File logic
-#TODO: make it so you can put any number of pdfs as input serve the data to dataframe logic
+# File logic
+# TODO: make it so you can put any number of pdfs as input serve the data to dataframe logic
 
 
-
-path = "pdf/AFTINIE GRIGORITA.pdf"
-pdfFile=open(path,"rb")
-
+directory = 'pdf'
+f = []
 
 
-viewer = PdfReader(pdfFile)
-text = viewer.pages[0].extract_text()
-# st.write(f"Asta e text:{text}")
-textArray = text.capitalize().split('\n')
-
-#functions
-@st.cache_data
-def findKey(keyword,text):
-    res=[]
+def findKey(keyword, text):
+    res = []
     mystring = text
-   
+
     before_keyword, keyword, after_keyword = mystring.partition(keyword)
     # st.write(f"before_keyword: {before_keyword}\n, keyword: {keyword}\n, after_keyword: {after_keyword}\n")
     res.append(before_keyword)
@@ -35,41 +28,52 @@ def findKey(keyword,text):
     res.append(after_keyword)
     return res
 
-#dataframe logic
-temp1 = findKey('Perioada internarii: ', text)
-temp2 = findKey('Medic: ', text)
-perioada=temp1[2].replace(temp2[2],'').replace(temp2[1],'')
-st.write(f"Perioada: {perioada}")
 
-nume=[]
-prenume=[]
-dataTiparire=[]
-chomp = text.split()
-# print("chomp:\n",chomp)
+def findField(key1, key2, text):
+    temp1 = findKey(key1, text)
+    temp2 = findKey(key2, text)
+    res = temp1[2].replace(temp2[2], '').replace(temp2[1], '')
+    st.write(f"res: {res}")
+    return res
 
-@st.cache_data
-def writeWord(array,i) :
-    return array[i]
+ 
+for filename in os.listdir(directory):
+    f.append(os.path.join(directory, filename))
+    # checking if it is a file
 
+st.write(f)
+metadata = np.empty([len(f),len(f)], dtype="<U200")
+for i in range(len(f)):
+    pdfFile = open(f[i], "rb")
+    viewer = PdfReader(pdfFile)
+    text = viewer.pages[0].extract_text()
 
-for i in range (len(textArray)-1):
-    st.write(f"chomp[{i}]: {writeWord(chomp,i)}")
-    if(str(chomp[i]) == "NUMELE"):  
-        nume.append(chomp[i+1])
-    if(str(chomp[i])  == "PRENUMELE"):
-        prenume.append(chomp[i+1])
-    if("tiparire" in chomp[i]):
-        dataTiparire.append(chomp[i+1])
-     
+   
 
-data = {
-    "Data Tiparire":dataTiparire,
-    "Nume":nume,
-    "Prenume": prenume,
-    "Perioada Internarii": perioada
-}
-df = pd.DataFrame(data)
+    st.write(f"TEXT:\n\n\n{text}")
 
-with pd.ExcelWriter("excel/output.xlsx") as writer:
-    df.to_excel(writer, sheet_name="Output", index=False)
-st.write(df)
+    # Keep in mind the j coordonates would eventually corespond to different patients
+    metadata[0][i] = findField("NUMELE ", "PRENUMELE", text)  # nume
+    metadata[1][i] = findField("PRENUMELE ", "VIRSTA", text)  # prenume
+
+    metadata[2][i] = findField(
+        "Data tiparire: ", "Sectia", text)  # dataTiparire
+
+    metadata[3][i] = findField("Perioada internarii: ",
+                               "Medic:", text)  # perioada
+
+    metadata[4][i] = findField("Urgenta ", "NUMELE ", text)
+    st.write(metadata)
+    data = {
+
+        "Nume": metadata[0],
+        "Prenume": metadata[1],
+        "Data Tiparire": metadata[2],
+        "Perioada Internarii": metadata[3],
+        "Urgenta": metadata[4]
+    }
+    df = pd.DataFrame(data)
+
+    with pd.ExcelWriter("excel/output.xlsx") as writer:
+        df.to_excel(writer, sheet_name="Output", index=False)
+    st.write(df)
